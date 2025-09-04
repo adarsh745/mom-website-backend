472ab6d5455437bb3fa1f653606bfcbf9fa19ba5
udai002 <karumuriudaisai002@gmail.com>
2025-09-04 15:44:34 +0530

    added leave form routes

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ connectDb()
 const app=express()
 app.use(cors({
     origin:"http://localhost:5173" ,
+    origin:"http://localhost:5174" ,
     credentials:true
 }))
 app.use(express.json())
--- routes/Employeeportal/leave.routes.js
+++ routes/Employeeportal/leave.routes.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const { applyLeave, getAllLeaves, approveLeave } = require("../../controllers/Employeeortal/leave.controller")
+const verifyEmployee = require("../../middlewares/VerifyEmployee")
 const router = express()
 
 //get all leaves for admin
 router.get("/leaves" ,  getAllLeaves)
 
 //apply leave by user 
-router.post("/leave/apply" , applyLeave)
+router.post("/leave/apply" , verifyEmployee, applyLeave)
 
 //approve leave by admin
 router.put("/leave/approve/:id" , approveLeave)
