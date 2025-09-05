e9266a74f4ad808a48ca0a00cbb1eefab381b52c
udai002 <karumuriudaisai002@gmail.com>
2025-09-05 10:20:40 +0530

    added routes for user leaves

--- middlewares/VerifyEmployee.js
+++ middlewares/VerifyEmployee.js
@@ -4,7 +4,7 @@ async function verifyEmployee(req,res,next){
 
     try{
       const token=req.headers.authorization.split(" ")[1]
-      console.log(token)
+      console.log("this is from middleware" , token)
       if(!token){
         return res.status(401).json({msg:"invalid access"})
       }
